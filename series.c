47be4c3243f20860edba8d664ff4f687b1681295
Adrian <kerpador@gmail.com>
2024-04-25 10:20:32 -0600

    Update series.c
    Añadidas más series

--- series.c
+++ series.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 double pi(int k);
+float inverse(int n ,int x);
+int factorial(int n);
+double sinF(int n, int k);
+float power(int n ,int x);
 int main(){
     double pin=pi(10000);
     printf("%lf",pin);
@@ -15,4 +19,28 @@ double pi(int k){
         return 4;
     else
         return (4*((double) signo)/(2*k+1))+pi(k-1);
+}
+float inverse(int n ,int x){
+    if(n==0)
+        return 1;
+    else 
+        return inverse(n-1,x)/(x);
+}
+float power(int n ,int x){
+    if(n==0)
+        return 1;
+    else 
+        return x*power(n-1,x);
+}
+int factorial(int n){
+    if(n==0)
+        return 1;
+    else
+        return n*factorial(n-1);
+}
+double sinF(int n,int x){
+    if (n==0)
+        return x;
+    else
+        return (power(2*n+1,x)/factorial(2*n+1))+sinF(n-1,x);
 }
